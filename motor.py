e98e22eaf78fabb59eadcd21ae4b372dfc9a21b3
AlwinYen 顏才翔 <alwinyen@users.noreply.github.com>
2017-06-04 23:24:03 +0800

    Update motor.py

--- motor.py
+++ motor.py
@@ -4,6 +4,11 @@ import time
 import math
 GPIO.setwarnings(False)
 GPIO.setmode(GPIO.BCM)
+GPIO.setup(21,GPIO.OUT)
+GPIO.setup(23,GPIO.OUT)
+GPIO.setup(25,GPIO.OUT)
+GPIO.setup(24,GPIO.OUT)
+GPIO.setup(12,GPIO.OUT)
 GPIO.setup(19,GPIO.OUT)
 GPIO.setup(13,GPIO.OUT)
 GPIO.setup(16,GPIO.OUT)
@@ -12,6 +17,15 @@ GPIO.setup(4,GPIO.OUT)
 GPIO.setup(17,GPIO.OUT)
 GPIO.setup(27,GPIO.OUT)
 GPIO.setup(22,GPIO.OUT)
+
+GPIO.output(23,GPIO.LOW)
+GPIO.output(24,GPIO.LOW)
+GPIO.output(25,GPIO.LOW)
+
+R = GPIO.PWM(24,100)
+G = GPIO.PWM(25,100)
+B = GPIO.PWM(23,100)
+
 
 FL_DIR = 4
 RL_DIR = 17
@@ -27,6 +41,8 @@ RL.start(0)
 FR.start(0)
 RR.start(0)
 FL.start(0)
+
+
 global vel
 vel = 0.0
 
@@ -180,7 +196,15 @@ def direction_f(direction):
 		
 	return
 			     
-def speed(vel_FL,vel_RL,vel_FR,vel_RR): 
+def speed(vel_FL,vel_RL,vel_FR,vel_RR):
+		if(vel_FL > 0 and vel_FL <= 10 or vel_RL > 0 and vel_RL <= 10 or vel_FR > 0 and vel_FR <= 10 or vel_RR > 0 and vel_RR <= 10 ):
+			vel_FL +=10
+			vel_RL +=10
+			vel_FR +=10
+			vel_RR +=10
+			vel = 10.0
+			app.setLabel("vel",vel)
+			
 		FL.start(vel_FL)
 		RL.start(vel_RL)
 		FR.start(vel_FR)
@@ -192,9 +216,26 @@ def stop(btn):
 	FR.stop()
 	RR.stop()
 
+def relay1(btn):
+	if GPIO.input(21) == True:
+		GPIO.output(21,GPIO.LOW)
+	else:
+		GPIO.output(21,GPIO.HIGH)
+
+def relay2(btn):
+	if GPIO.input(12) == True:
+		GPIO.output(12,GPIO.LOW)
+	else:
+		GPIO.output(12,GPIO.HIGH)	
+
+def color_change():	
+	R.start(app.getScale("Red"))
+	G.start(app.getScale("Green"))
+	B.start(app.getScale("Blue"))
+	
 try:   
-	app=gui("Control","600x300")
-	app.setFont(20)
+	app=gui("Control","600x500")
+	app.setFont(17)
 	app.addLabel("Status","Status:")
 	app.addLabel("Direction","None")
 	app.addLabel("Moving","Moving Direction:")
@@ -202,11 +243,26 @@ try:
 	app.addLabel("Speed","Speed:")
 	app.addLabel("vel","0")
 	app.setLabel("vel",vel)
+	app.addLabelScale("Red")
+	app.setScaleRange("Red",0,100,curr=None)
+	app.showScaleValue("Red",show= True)
+	app.addLabelScale("Green")
+	app.setScaleRange("Green",0,100,curr=None)
+	app.showScaleValue("Green",show= True)
+	app.addLabelScale("Blue")
+	app.setScaleRange("Blue",0,100,curr=None)
+	app.showScaleValue("Blue",show= True)
 	app.bindKey("a", left)
 	app.bindKey("d", right)
 	app.bindKey("w", foward_f)
 	app.bindKey("s", backward_f)
 	app.bindKey("x", stop_f)
+	app.bindKey("1",relay1)
+	app.bindKey("2",relay2)
+	app.bindKey("r",red_on)
+	app.bindKey("g",green_on)
+	app.bindKey("b",blue_on)
+	app.registerEvent(color_change)
 	app.go()
 	GPIO.cleanup()
 	
